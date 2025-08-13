d90ee734299f222fb1320c706672df01eb020558
24ftt1813 <24FTT1813@student.pb.edu.bn>
2025-08-13 22:11:34 +0800

    Add files via upload

---
+++ P2/DisplayTwoMessage.java
@@ -0,0 +1,5 @@
+public class DisplayTwoMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to AD2204 Introduction to Programming\nThis is Practical 2");
+    }
+}
